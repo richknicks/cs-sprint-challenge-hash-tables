e0a7c07bc45491f495a3daa246b9033b8f3d907c
richknicks <rlynch@gmail.com>
2020-07-10 14:24:11 -0400

    replaced ex 1 with ex2 for more confident answer passing test

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -4,23 +4,3 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     # Your code here
 
-    cache = {}
-    for i in range(length):
-        if weights[i] not in cache:
-            cache[weights[i]] = [i]
-        else:
-            cache[weights[i]].append(i)
-
-    for weight in weights:
-        pair = limit - weight
-        if pair not in cache:
-            continue
-        elif pair == weight:
-            return [cache[weight][1], cache[weight][0]]
-        else:
-            if cache[weight] > cache[pair]:
-                return [cache[weight][0], cache[pair][0]]
-            else:
-                return [cache[pair][0], cache[weight][0]]
-    return None
-
